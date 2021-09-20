5f2d42dd82f2cd03cbd708a1dcb4107637bf5bad
Laurens Verschuere <laurens@massivemedia.eu>
2021-09-20 16:52:53 +0200

    lint fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,10 @@ import './bootstrap';
 
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
 import minimist from 'minimist';
-import fs from 'fs'
-import http from 'http'
-import https from 'https'
 
 import schema from './graphql/schema';
 import snapshot from './proxy/snapshot';
@@ -21,7 +21,7 @@ const apollo = new ApolloServer({
   schema,
 });
 
-const useSSL = !!process.env.SSL
+const useSSL = !!process.env.SSL;
 
 // without this, apollo will throw an error.
 apollo.start().then(() => {
@@ -35,16 +35,19 @@ apollo.start().then(() => {
   apollo.applyMiddleware({ app, path: '/' });
 
   // Start web server
-  let webServer: http.Server | https.Server
+  let webServer: http.Server | https.Server;
 
   if (useSSL) {
     if (!fs.existsSync('/ssl/fullchain.pem') || !fs.existsSync('/ssl/privkey.pem')) {
-      throw Error("SSL files not found")
+      throw Error('SSL files not found');
     }
-    webServer = https.createServer({
-      key: fs.readFileSync('/ssl/privkey.pem'),
-      cert: fs.readFileSync('/ssl/fullchain.pem'),
-    }, app);
+    webServer = https.createServer(
+      {
+        key: fs.readFileSync('/ssl/privkey.pem'),
+        cert: fs.readFileSync('/ssl/fullchain.pem'),
+      },
+      app
+    );
   } else {
     webServer = http.createServer(app);
   }
