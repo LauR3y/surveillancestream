b822ea0d6821bdf5aae13c4675d066b267248e96
Laurens Verschuere <laurens@massivemedia.eu>
2021-09-27 17:04:28 +0200

    Set images/path config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['localhost', '127.0.0.1'],
+    path: process.env.NEXT_PUBLIC_ASSET_PREFIX,
   },
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
 }
