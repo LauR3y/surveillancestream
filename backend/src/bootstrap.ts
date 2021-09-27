0949359faee33e28db57d4042ed1d63ea8bba57c
Laurens Verschuere <laurens@massivemedia.eu>
2021-09-27 13:15:09 +0200

    improve error logging

--- backend/src/bootstrap.ts
+++ backend/src/bootstrap.ts
@@ -1,11 +1,12 @@
 import { config as dotenv } from 'dotenv';
 
 // log errors to stderr
-const consolePrinter = (err: any) => {
-  console.error(err && err.stack ? err.stack : err);
-};
-process.on('uncaughtException', consolePrinter);
-process.on('unhandledRejection', consolePrinter);
+process.on('uncaughtException', (error: Error, origin: 'uncaughtException' | 'unhandledRejection') => {
+  console.error(error && error.stack ? error.stack : error, origin);
+});
+process.on('unhandledRejection', (reason: Record<string, unknown> | null | undefined, promise: Promise<unknown>) => {
+  console.error(reason, promise);
+});
 
 // load environment variables, fallback to .env
 dotenv();
